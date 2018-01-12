0eb1f16a1dfc86b2589486ed15910170bd0d324b
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-13 02:14:57 +0900

    Delete old files.

--- Levolution.Data.Timeline.Shared/TimelineGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Levolution.Data.Timeline
-{
-    public class TimelineGroup : Timeline
-    {
-        public ICollection<Timeline> Children { get; protected set; } = new List<Timeline>();
-    }
-}
