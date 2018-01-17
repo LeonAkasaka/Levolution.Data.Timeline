1a020f6b9de417161bac41debe0fdf0a9a4a2027
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-17 23:25:35 +0900

    Add sample project.

---
+++ Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Levolution.Data.Timeline;
+using System;
+using System.Threading;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new ImmediateClock();
+            var t = new IntegerTimeline()
+            {
+                From = 0,
+                To = 100,
+                Clock = clock,
+                Duration = new Duration(TimeSpan.FromSeconds(5))
+            };
+
+            var startTime = DateTime.Now;
+            t.Play();
+
+            while(t.CurrentState == TimelineState.Playing)
+            {
+                clock.Update();
+                Console.WriteLine(t.Value);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine($"Time: {(DateTime.Now - startTime).TotalMilliseconds}/ms");
+        }
+    }
+}
